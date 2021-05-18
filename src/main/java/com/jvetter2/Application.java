bb327b6a5857e7e964d97a4978d5f697b5995825
Jordan Vetter <jordan_vetter@optum.com>
2021-05-18 12:50:08 -0500

    Removed unused imports

--- src/main/java/com/jvetter2/Application.java
+++ src/main/java/com/jvetter2/Application.java
@@ -3,14 +3,7 @@ package com.jvetter2;
 import com.jvetter2.configuration.WebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-//@ComponentScan(basePackages={"com.jvetter2"})
-//@EntityScan(basePackages={"com.jvetter2"})
-//@EnableCr(basePackages={"com.jvetter2"})
 
 @SpringBootApplication
 public class Application extends AbstractAnnotationConfigDispatcherServletInitializer {
--- src/main/java/com/jvetter2/controller/ThymeController.java
+++ src/main/java/com/jvetter2/controller/ThymeController.java
@@ -1,12 +1,13 @@
 package com.jvetter2.controller;
 
 import com.jvetter2.Subscription;
-import com.jvetter2.SubscriptionController;
-import com.jvetter2.SubscriptionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
